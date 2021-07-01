f91b0b713a474a2005f13ab61f69e07a51b095c7
Fabrizio Cacicia <mail@fabriziocacicia.com>
2021-07-01 12:54:01 +0200

    Add a custom toString implementation to DependencyNotFoundException

--- lib/src/exceptions.dart
+++ lib/src/exceptions.dart
@@ -2,4 +2,7 @@ class DependencyNotFoundException implements Exception {
   final String message;
 
   DependencyNotFoundException({required final this.message});
+
+  @override
+  String toString() => "DependencyNotFoundException: $message";
 }
