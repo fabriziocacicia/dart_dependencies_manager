784f19e7145e3794323b27ebb8a22d9bfcc904af
Fabrizio Cacicia <mail@fabriziocacicia.com>
2021-07-01 13:06:10 +0200

    Add more informations to the message of DependencyNotFoundException

--- lib/src/exceptions.dart
+++ lib/src/exceptions.dart
@@ -3,6 +3,15 @@ class DependencyNotFoundException implements Exception {
 
   DependencyNotFoundException({required final this.message});
 
+  String  get _parsedGetItAssertionErrorMessage {
+    final splittedMessage = message.split(' ');
+    final indexOfType = splittedMessage.indexOf('type');
+    return splittedMessage[indexOfType + 1];
+  }
+
+
   @override
-  String toString() => "DependencyNotFoundException: $message";
+  String toString() => "DependencyNotFoundException: "
+      "$_parsedGetItAssertionErrorMessage not found.\nCheck if you forgot "
+      "to register it or if you need to retrieve it asynchronously.";
 }
